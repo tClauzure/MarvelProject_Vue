b354a4028b2066e497ab18727450a7c99d669c25
tclauzure <theo.clauzure@outlook.fr>
2024-02-28 21:08:20 +0100

    search bar component

---
+++ src/components/TheSearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <input type="text" v-model="searchQuery" @input="handleInput" placeholder="Rechercher..." />
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref, onMounted, onUnmounted, PropType } from 'vue';
+  
+  export default defineComponent({
+    name: 'SearchBar',
+    props: {
+      onSearch: {
+        type: Function as PropType<(query: string) => void>,
+        required: true
+      }
+    },
+    setup(props) {
+      const searchQuery = ref('');
+  
+      const handleInput = () => {
+        props.onSearch(searchQuery.value.trim());
+      };
+  
+      return {
+        searchQuery,
+        handleInput
+      };
+    }
+  });
+  </script>
+  
